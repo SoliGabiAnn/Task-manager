b497c37297940294b4dd1e2d41d53aaa9084ffb1
aslusarczyk <aga.slu.24@gmail.com>
2024-10-15 11:11:38 +0200

    Zadanie

---
+++ Lab2/src/Lotto.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Lotto {
+    public static void main(String[] args){
+
+//        int n = 6;
+//        ArrayList<Integer> arr1 = new ArrayList<Integer>(n);
+//
+//        for (int i = 1; i <= n; i++) {
+//            int randomNum = (int)(Math.random() * 49);
+//            int x = randomNum + 1;
+//            if (arr1.contains(x) == false) {
+//                arr1.add(x);
+//
+//            }else{
+//                i -= 1;
+//            }
+//        }
+        System.out.println("Length of your list = "+args.length);
+
+        for(int i = 0; i< args.length; i++) {
+            System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));
+        }
+    }
+}
