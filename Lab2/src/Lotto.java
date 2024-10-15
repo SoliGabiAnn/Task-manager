4ec22100c118d19b39d4eee8dfe1f906858fe6cf
aslusarczyk <aga.slu.24@gmail.com>
2024-10-15 11:14:13 +0200

    Zadanie

--- Lab2/src/Lotto.java
+++ Lab2/src/Lotto.java
@@ -1,24 +1,23 @@
 import java.util.*;
-import java.io.*;
 package com.journaldev.examples;
 
 
 public class Lotto {
     public static void main(String[] args){
 
-//        int n = 6;
-//        ArrayList<Integer> arr1 = new ArrayList<Integer>(n);
-//
-//        for (int i = 1; i <= n; i++) {
-//            int randomNum = (int)(Math.random() * 49);
-//            int x = randomNum + 1;
-//            if (arr1.contains(x) == false) {
-//                arr1.add(x);
-//
-//            }else{
-//                i -= 1;
-//            }
-//        }
+        int n = 6;
+        ArrayList<Integer> arr1 = new ArrayList<Integer>(n);
+
+        for (int i = 1; i <= n; i++) {
+            int randomNum = (int)(Math.random() * 49);
+            int x = randomNum + 1;
+            if (arr1.contains(x) == false) {
+                arr1.add(x);
+
+            }else{
+                i -= 1;
+            }
+        }
         System.out.println("Length of your list = "+args.length);
 
         for(int i = 0; i< args.length; i++) {
