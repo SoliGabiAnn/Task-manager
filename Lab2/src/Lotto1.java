88227c78ddeead6dd53e7d4ab2641c7fe48b4027
Agata Ślusarczyk <aslusarczyk@student.agh.edu.pl>
2024-10-17 19:07:42 +0200

    Zadanie 2

---
+++ Lab2/src/Lotto1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Lotto1 {
+    public static void main(String[] args) {
+        System.out.println("Length of your list = " + args.length);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(args.length == 6) {
+            for(int i = 0; i< args.length; i++) {
+                int number = Integer.parseInt(args[i]);
+                if((number < 50) & (number > 0 )){
+                    if(!list.contains(number)) {
+                        list.add(number);
+                        System.out.println(args[i]);
+                    }
+                }else{
+                    System.out.println("Podaj inny argument");
+                }
+            }
+        }else {
+            System.out.println("Ilość liczb się nie zgadza. Podaj ich 6!");
+        }
+
+
+        System.out.println("Twoje liczby: " + list);
+
+
+//        for(int i = 0; i< args.length; i++) {
+//            if(args[i] < 50 & args[i] > 0 ){
+//                System.out.println(args[i]);
+//            }
+////            System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));
+//        }
+//    }
+
+    }
+}
+
+
