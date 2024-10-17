e62990a9ae6c5a53efebc2472bbc2815927220ff
Agata Ślusarczyk <aslusarczyk@student.agh.edu.pl>
2024-10-17 19:22:45 +0200

    Zadanie 2

--- Lab2/src/Lotto1.java
+++ Lab2/src/Lotto1.java
@@ -2,38 +2,28 @@ import java.util.*;
 
 public class Lotto1 {
     public static void main(String[] args) {
-        System.out.println("Length of your list = " + args.length);
         ArrayList<Integer> list = new ArrayList<>();
 
-        if(args.length == 6) {
+        System.out.println("Podaj 6 liczb w zakresie od 1 do 49");
 
+        if(args.length == 6) {
             for(int i = 0; i< args.length; i++) {
                 int number = Integer.parseInt(args[i]);
                 if((number < 50) & (number > 0 )){
                     if(!list.contains(number)) {
                         list.add(number);
                         System.out.println(args[i]);
+                    }else{
+                        System.out.println("Twoja liczba się powtórzyła. Podaj inny argument");
                     }
                 }else{
-                    System.out.println("Podaj inny argument");
+                    System.out.println("Podany argument nie mieści się w przedziale 1-49. Podaj inną liczbę");
                 }
             }
+            System.out.println("Twoje liczby: " + list);
         }else {
             System.out.println("Ilość liczb się nie zgadza. Podaj ich 6!");
         }
-
-
-        System.out.println("Twoje liczby: " + list);
-
-
-//        for(int i = 0; i< args.length; i++) {
-//            if(args[i] < 50 & args[i] > 0 ){
-//                System.out.println(args[i]);
-//            }
-////            System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));
-//        }
-//    }
-
     }
 }
 
